442c97d64f946ed76835b10d261f5adaca48273a
Aron Carroll <self@aroncarroll.com>
2014-06-20 15:13:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+/* A simple config object for a node application. Accepts two objects, the
+ * first is a set of defaults. The second is an environment object. All app
+ * specific environment variables can have a prefix this will be stripped by
+ * the config.
+ *
+ * If a function is used for a config value this will be evaluated at
+ * runtime providing access to the entire config object as an argument.
+ */
+'use strict';
+
+var PRODUCTION = 'production';
+var DEVELOPMENT = 'development';
+var TEST = 'test';
+var INTEGRATION = 'integration';
+
+exports.PRODUCTION = PRODUCTION;
+exports.DEVELOPMENT = DEVELOPMENT;
+exports.TEST = TEST;
+exports.INTEGRATION = INTEGRATION;
+
+exports.Config = function Config(values, env, options) {
+    var config = {};
+    env = env || {};
+    options = defaults(options, {
+        prefix: '',
+        unprefixed: ['PORT', 'NODE_ENV'],
+        environments: {
+            PRODUCTION: PRODUCTION,
+            DEVELOPMENT: DEVELOPMENT,
+            TEST: TEST,
+            INTEGRATION: INTEGRATION
+        }
+    });
+
+    Object.keys(values).forEach(function (key) {
+        var envVar = prefix(key);
+        var envValue = has(env, envVar) ? coerce(env[envVar]) : null;
+        config[key] = envValue !== null ? envValue : values[key];
+    });
+
+    this.PRODUCTION = options.environments.PRODUCTION;
+    this.DEVELOPMENT = options.environments.DEVELOPMENT;
+    this.TEST = options.environments.TEST;
+    this.INTEGRATION = options.environments.INTEGRATION;
+
+    this.get = function get(key, fallback) {
+        var hasKey = has(config, key);
+        var hasFallback = arguments.length === 2;
+        var value = hasKey ? config[key] : fallback;
+
+        if (!hasKey && !hasFallback) {
+            throw new Error('Missing config key: ' + key);
+        }
+
+        return typeof value === 'function' ? value(this) : value;
+    };
+
+    this.isProduction = function isProduction() {
+        return this.get('NODE_ENV') === this.PRODUCTION;
+    };
+
+    this.isDevelopment = function isDevelopment() {
+        return this.get('NODE_ENV') === this.DEVELOPMENT;
+    };
+
+    this.isTest = function isTest() {
+        return this.get('NODE_ENV') === this.TEST;
+    };
+
+    this.isIntegration = function isIntegration() {
+        return this.get('NODE_ENV') === this.INTEGRATION;
+    };
+
+    this.isTestOrIntegration = function isTestOrIntegration() {
+        return this.isTest() || this.isIntegration();
+    };
+
+    this.version = function version() {
+        if (!options.version) {
+            throw new Error('Version must be provided when instantiating Config');
+        }
+        return options.version;
+    };
+
+    function prefix(key) {
+        var isUnprefixed = options.unprefixed.indexOf(key) > -1;
+        var prefixed = options.prefix ? options.prefix.toUpperCase() + '_'  + key : key;
+        return isUnprefixed ? key : prefixed;
+    }
+};
+
+function has(obj, key) {
+    return !!obj && Object.prototype.hasOwnProperty.call(obj, key);
+}
+
+function defaults(obj, def) {
+    var clone = {};
+    var key;
+
+    for (key in obj) {
+        if (has(obj, key)) {
+            clone[key] = obj[key];
+        }
+    }
+
+    for (key in def) {
+        clone[key] = has(obj, key) ? obj[key] : def[key];
+    }
+
+    return clone;
+}
+
+function coerce(value) {
+    if (value === 'true') {
+        return true;
+    }
+    if (value === 'false') {
+        return false;
+    }
+    if (/^\d+$/.test(value)) {
+        return parseInt(value, 10);
+    }
+    return value;
+}
---
+++ test/index_test.js
@@ -0,0 +1,158 @@
+'use strict';
+
+var sinon = require('sinon');
+var assert = require('assert');
+var Config = require('../index').Config;
+
+describe('config', function () {
+    var DEFAULT_CONFIG = {
+        PORT: 4567,
+        NODE_ENV: 'production',
+        DEFAULT_VALUE: 'default',
+        IS_TRUE: null,
+        IS_FALSE: null,
+        NUMBER: null
+    };
+
+    function createConfig(env, options) {
+        return new Config(DEFAULT_CONFIG, env, options);
+    }
+
+    it('exposes each of the environment constants', function () {
+        var config = createConfig();
+        assert.equal(config.PRODUCTION, 'production');
+        assert.equal(config.DEVELOPMENT, 'development');
+        assert.equal(config.TEST, 'test');
+    });
+
+    describe('.get', function () {
+        it('returns the value from the defaults object', function () {
+            var config = createConfig();
+            assert.equal(config.get('DEFAULT_VALUE'), 'default');
+        });
+
+        it('returns the value from the defaults object', function () {
+            var config = createConfig();
+            assert.throws(function () {
+                config.get('MISSING_VALUE');
+            }, Error);
+        });
+
+        it('allows a fallback value to be provided', function () {
+            var config = createConfig();
+            assert.equal(config.get('MISSING_VALUE', 'found'), 'found');
+        });
+
+        it('returns the result of a value if a function is provided', function () {
+            var value = sinon.stub().returns('callback');
+            var config = new Config({LAZY_VALUE: value});
+            assert.equal(config.get('LAZY_VALUE'), 'callback');
+            sinon.assert.calledWith(value, config);
+        });
+
+        it('overrides existing options with environment variables', function () {
+            var config = createConfig({DEFAULT_VALUE: 'overidden'});
+            assert.equal(config.get('DEFAULT_VALUE'), 'overidden');
+        });
+
+        it('can be configured to use a prefix when looking for environment variables', function () {
+            var config = createConfig({MYAPP_DEFAULT_VALUE: 'overidden'}, {prefix: 'MYAPP'});
+            assert.equal(config.get('DEFAULT_VALUE'), 'overidden');
+        });
+
+        it('allows overidding of special variables without a prefix', function () {
+            var config = createConfig({NODE_ENV: 'test', PORT: 1984}, {prefix: 'MYAPP'});
+            assert.equal(config.get('NODE_ENV'), 'test');
+            assert.equal(config.get('PORT'), 1984);
+        });
+
+        it('allows special unprefix variables to be provided', function () {
+            var config = createConfig({DEFAULT_VALUE: 'overidden'}, {
+                prefix: 'MYAPP',
+                unprefixed: ['DEFAULT_VALUE']
+            });
+            assert.equal(config.get('DEFAULT_VALUE'), 'overidden');
+        });
+
+        it('coreces "true" and "false" environment variables into booleans', function () {
+            var config = createConfig({IS_TRUE: 'true', IS_FALSE: 'false'});
+            assert.strictEqual(config.get('IS_TRUE'), true);
+            assert.strictEqual(config.get('IS_FALSE'), false);
+        });
+
+        it('coerces numeric strings into integers', function () {
+            var config = createConfig({NUMBER: '2046'});
+            assert.strictEqual(config.get('NUMBER'), 2046);
+        });
+
+        it('does not coerce alpha-numeric strings into integers', function () {
+            var config = createConfig({NUMBER: 'http://localhost:2046'});
+            assert.strictEqual(config.get('NUMBER'), 'http://localhost:2046');
+        });
+    });
+
+    describe('.isProduction', function () {
+        it('returns true if the node environment is set to production', function () {
+            var config = createConfig({NODE_ENV: 'production'});
+            assert.equal(config.isProduction(), true);
+        });
+
+        it('returns false if the node environment is not set to production', function () {
+            var config = createConfig({NODE_ENV: 'development'});
+            assert.equal(config.isProduction(), false);
+        });
+    });
+
+    describe('.isDevelopment', function () {
+        it('returns true if the node environment is set to development', function () {
+            var config = createConfig({NODE_ENV: 'development'});
+            assert.equal(config.isDevelopment(), true);
+        });
+
+        it('returns false if the node environment is not set to development', function () {
+            var config = createConfig();
+            assert.equal(config.isDevelopment(), false);
+        });
+    });
+
+    describe('.isTest', function () {
+        it('returns true if the node environment is set to test', function () {
+            var config = createConfig({NODE_ENV: 'test'});
+            assert.equal(config.isTest(), true);
+        });
+
+        it('returns false if the node environment is not set to test', function () {
+            var config = createConfig();
+            assert.equal(config.isTest(), false);
+        });
+    });
+
+    describe('.isIntegration', function () {
+        it('returns true if the node environment is set to integration', function () {
+            var config = createConfig({NODE_ENV: 'integration'});
+            assert.equal(config.isIntegration(), true);
+        });
+
+        it('returns false if the node environment is not set to integration', function () {
+            var config = createConfig();
+            assert.equal(config.isIntegration(), false);
+        });
+    });
+
+    describe('.isTestOrIntegration', function () {
+        it('returns true if the node environment is set to integration', function () {
+            var config = createConfig({NODE_ENV: 'integration'});
+            assert.equal(config.isTestOrIntegration(), true);
+        });
+
+        it('returns true if the node environment is set to test', function () {
+            var config = createConfig({NODE_ENV: 'test'});
+            assert.equal(config.isTestOrIntegration(), true);
+        });
+
+        it('returns false if the node environment is not set to integration', function () {
+            var config = createConfig();
+            assert.equal(config.isTestOrIntegration(), false);
+        });
+    });
+});
